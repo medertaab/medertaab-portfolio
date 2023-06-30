f8f4f51260f392dcc19c39c2030641aa20dc6a40
Meder <medertaab@gmail.com>
2023-06-30 17:26:26 +0600

    Change Index decoration

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,24 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Gradient from "./gradient";
 import styles from "./Hero.module.scss";
 import Name from "./name";
 import SubHeader from "./subHeader";
+import StarFall from "./starFall";
 
 export default function Hero() {
-  const [animating, setAnimating] = useState(false);
-  
+  const [bgMoving, setBgMoving] = useState(false);
+  const [starFall, setStarFall] = useState(false);
+
+  function handleStarFall() {
+    setStarFall(true)
+    setTimeout(() => {
+      setStarFall(false)
+    }, 2500)
+  }
+
   return (
     <section className={styles.heroSection} id="main">
-      {!animating && (
+      {!bgMoving && (
         <img
           src="/images/gradient_bg.jpg"
           style={{ width: "100%", position: "absolute", top: "0" }}
           className={styles.staticBg}
         ></img>
       )}
-      <Name animating={animating} setAnimating={setAnimating} />
+      <Name
+        bgMoving={bgMoving}
+        setBgMoving={setBgMoving}
+        starFall={starFall}
+        handleStarFall={handleStarFall}
+      />
       <SubHeader />
-      {animating && <Gradient />}
+      {bgMoving && <Gradient />}
+      {starFall && <StarFall />}
     </section>
   );
 }
--- src/components/hero/name/index.tsx
+++ src/components/hero/name/index.tsx
@@ -1,28 +1,24 @@
-import React, { ReactPropTypes, useEffect } from "react";
+import React from "react";
 import styles from "./Name.module.scss";
 import Sparkle from "@public/svg/sparkle.svg"
 import ReactLogo from "@public/svg/reactlogo.svg"
-import ShootingStars from "../shootingStars";
 
-export default function Name({animating, setAnimating} : any) {
+export default function Name({bgMoving, setBgMoving, starFall, handleStarFall} : any) {
+
   return (
     <div className={styles.name}>
       <div>
         <h1 className={styles.firstName}>MEDER</h1>
-        <button className={styles.starButton}>
+        <button disabled={starFall} className={styles.starButton} onClick={handleStarFall}>
           <Sparkle />
         </button>
       </div>
       <div>
-        <button className={styles.reactButton} onClick={() => setAnimating(!animating)}>
+        <button className={styles.reactButton} onClick={() => setBgMoving(!bgMoving)}>
           <ReactLogo />
         </button>
         <h1>TAAB</h1>
       </div>
-
-      <ShootingStars left/>
-      <ShootingStars right/>
-      <ShootingStars top/>
     </div>
   );
 }
