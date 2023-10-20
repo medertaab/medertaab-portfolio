e0e7e0dd7ff39300f73fd13f343b912e66f60876
Meder <medertaab@gmail.com>
2023-10-20 21:50:00 +0600

    Change About section layout

--- src/components/about/index.tsx
+++ src/components/about/index.tsx
@@ -8,21 +8,27 @@ export default function About() {
     <section className={styles.aboutSection}>
       <SectionTitle>About</SectionTitle>
 
-      <p>Hi! My name is Meder Taab and I create web stuff 🔮</p>
+      <div className={styles.textBlock}>
+        <p>Hi! My name is Meder Taab and I create web stuff 🔮</p>
 
-      <p>
-        I'm a creative professional working in webdev with a focus on <strong>front-end development</strong>. I enjoy creating delightful, engaging and accessible experiences for web users.
-      </p>
+        <p>
+          I'm a creative professional working in webdev with a focus on{" "}
+          <strong>front-end development</strong>. I enjoy creating delightful,
+          engaging and accessible experiences for web users.
+        </p>
 
-      <p>
-        With a lifelong interest in tech and the web, and over a decade worth of experience in creative industries like illustration and writing, I strive to combine strong senses of <strong>creativity and logic</strong> in my work.
-      </p>
+        <p>
+          With a lifelong interest in tech and the web, and over a decade worth
+          of experience in creative industries like illustration and writing, I
+          strive to combine strong senses of{" "}
+          <strong>creativity and logic</strong> in my work.
+        </p>
 
-      <p>
-        I'm eager to use my skills contribute to new projects, utilize my unique background and explore new opportunities!
-      </p>
-
-      <p>What I use:</p>
+        <p>
+          I'm eager to use my skills contribute to new projects, utilize my
+          unique background and explore new opportunities!
+        </p>
+      </div>
       <Stack />
     </section>
   );
